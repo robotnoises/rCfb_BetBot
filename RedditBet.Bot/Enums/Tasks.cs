4c656689ba97b0fe368e0811a19cc91c72d3fb33
davenich <davenich@gmail.com>
2014-12-17 13:17:29 -0500

    Added missing Tasks.cs

---
+++ RedditBet.Bot/Enums/Tasks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditBet.Bot.Enums
+{
+    public enum TaskType
+    {
+        Crawl,
+        Comment,
+        Update,
+        Message
+    }
+}

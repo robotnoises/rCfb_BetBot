658f0fc13fef5f0661ae18e5a11e745344e9de5b
davenich <davenich@gmail.com>
2014-12-17 21:53:54 -0500

    Factory to create Task from TaskType

---
+++ RedditBet.Bot/Tasks/TaskFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RedditBet.Bot.Enums;
+using RedditBet.Bot.Models;
+
+namespace RedditBet.Bot.Tasks
+{
+    public static class TaskFactory
+    {
+        public static IBotTask Create(BotTask task)
+        {
+            switch (task.TaskType)
+            { 
+                case TaskType.DirectMessage:
+                    return new DirectMessage(task);
+                case TaskType.Reply:
+                    return new Reply(task);
+                case TaskType.Update:
+                    return new UpdateReply(task);
+                default:
+                    return new Crawl();
+            }
+        }
+    }
+}

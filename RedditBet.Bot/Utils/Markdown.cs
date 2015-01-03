0dcb7ee5dc763d4bcd515bfd6d8d2475592fed6f
davenich <davenich@gmail.com>
2015-01-02 21:44:22 -0500

    Added basic Markdown support

---
+++ RedditBet.Bot/Utils/Markdown.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditBet.Bot.Utils
+{
+    public static class Markdown
+    {
+        public static string LINE_BREAK = "\n\n";
+
+        public static string H1 (string text) {
+            return string.Format("#{0}", text);
+        }
+        
+        public static string H2(string text)
+        {
+            return string.Format("##{0}", text);
+        }
+
+        public static string H3(string text)
+        {
+            return string.Format("###{0}", text);
+        }
+
+        public static string H4(string text)
+        {
+            return string.Format("####{0}", text);
+        }
+
+        public static string H5(string text)
+        {
+            return string.Format("#####{0}", text);
+        }
+
+        public static string UNORDERED_LIST(string[] listItems)
+        {
+            var sb = new StringBuilder(500);
+
+            foreach (var item in listItems)
+            {
+                sb.Append(string.Format("* {0}{1}", item, LINE_BREAK));
+                // sb.AppendLine(string.Format("* {0}", item));
+            }
+
+            return sb.ToString();
+        }
+        
+        public static string LINK(string text, string url)
+        {
+            return string.Format("[{0}]({1})", text, url);
+        }
+    }
+}
---
+++ RedditBet.Bot/Utils/Message.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditBet.Bot.Utils
+{
+    public static class Message
+    {
+        public static string Test()
+        {
+            var header = Markdown.H2("Header");
+            var anotherHeader = Markdown.H4("Here's a smaller header...");
+            var para = "This is a paragraph containing information.";
+            var para2 = "Hey, a second paragraph.";
+            var listOfThings = Markdown.UNORDERED_LIST(new string[] { "one","2","III","FORE!" });
+
+            return Compose(new string[] {header, anotherHeader, para, para2, listOfThings});
+        }
+
+        private static string Compose(string[] messageParts)
+        {
+            var sb = new StringBuilder(500);
+
+            for (var i = 0; i < messageParts.Length; i++)
+            {
+                sb.Append(messageParts[i] + Markdown.LINE_BREAK);
+            }
+
+            return sb.ToString();
+        }
+    }
+}

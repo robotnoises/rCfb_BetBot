213de285dce8ed65865c71a70640c53d62723bd9
nicholsd <nicholsd@opentext.com>
2015-01-21 10:27:31 -0500

    cleanup

--- RedditBet.API/Data/DbContext.cs
+++ RedditBet.API/Data/DbContext.cs
@@ -1,33 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Data.Entity;
-using RedditBet.API.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace RedditBet.API.Data
 {
+    using RedditBet.API.Models;
+
     [DbConfigurationType("RedditBet.API.App_Start.DbConfig, RedditBet.API")]
-    public class RedditBetDataContext : DbContext // : IdentityDbContext<ApplicationUser>
+    public class RedditBetDataContext : DbContext
     {
         // Construct Context using "RedditBet" as the conn string name
-        // public RedditBetDataContext() : base("RedditBet", throwIfV1Schema: false) { }
-        
         public RedditBetDataContext() : base("RedditBet") { }
 
         // Entities
         public DbSet<BotTask> Tasks { get; set; }
         public DbSet<Log> Logs { get; set; }
         public DbSet<BlacklistEntry> Blacklist { get; set; }
-
     }
 
     public static class DatabaseContext
     {
         public static RedditBetDataContext Create()
         {
-            // Todo: check here if context is disposed of? Created?
             return new RedditBetDataContext();
         }
     }

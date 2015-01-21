19a4992bc58815da020bedcb9beaaac0f2915591
nicholsd <nicholsd@opentext.com>
2015-01-21 11:45:03 -0500

    Added model for Bets

---
+++ RedditBet.API/Models/BetModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RedditBet.API.Models
+{
+    public class Bet
+    {
+        [Key]
+        public int BetId { get; set; }
+        [Required]
+        public string Solicitor { get; set; }
+        public string Challenger { get; set; }
+        [Required]
+        public string Terms { get; set; }
+        public bool IsConfirmed { get; set; }
+        [Required]
+        public DateTime EventDate { get; set; }
+        public DateTime? CutoffDate { get; set; }
+    }
+}

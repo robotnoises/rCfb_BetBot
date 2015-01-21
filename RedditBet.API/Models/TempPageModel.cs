b2c033d2771b18bc7556ab61089c3d17eb7c1a35
nicholsd <nicholsd@opentext.com>
2015-01-21 11:44:39 -0500

    Added model for TempPages

---
+++ RedditBet.API/Models/TempPageModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RedditBet.API.Models
+{
+    internal class TempPage
+    {
+        [Key]
+        public int TempPageId { get; set; }
+        [Required]
+        public string Token { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool Visited { get; set; }
+        [Required]
+        public virtual Bet AssociatedBet { get; set; }
+
+        private const int days_until_stale = 5;
+
+        public bool IsFresh()
+        {
+            return (!Visited && CreatedAt.AddDays(days_until_stale) <= DateTime.UtcNow);
+        }
+
+    }
+}

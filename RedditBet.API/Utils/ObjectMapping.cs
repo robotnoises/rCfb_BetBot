3a6c1a6a1f6ef181398696344f967aab151f0829
nicholsd <nicholsd@opentext.com>
2015-01-23 11:22:40 -0500

    Added a Mappable base class that allows easy mapping from one type to another

--- RedditBet.API/Utils/ObjectMapping.cs
+++ RedditBet.API/Utils/ObjectMapping.cs
@@ -8,7 +8,7 @@ namespace RedditBet.API
     public interface IMappable<OutT> 
         where OutT : class
     {
-        OutT ToDomainModel();
+        OutT ToMappedType();
     }
 
     public abstract class Mappable<T, OutT> : IMappable<OutT>
@@ -20,7 +20,12 @@ namespace RedditBet.API
             get { return (T)this; }
         }
 
-        public OutT ToDomainModel()
+        /// <summary>
+        /// When a derived class inherits from Mappable, this method allows auto-mapping to its "sibling" class, 
+        /// which is usually ViewModel -> DomainModel or DomainModel -> ViewModel
+        /// </summary>
+        /// <returns></returns>
+        public OutT ToMappedType()
         {
             try
             {
@@ -29,7 +34,7 @@ namespace RedditBet.API
             }
             catch (Exception ex)
             { 
-                // Todo throw exception
+                // Todo: throw exception
                 return null;
             }
         }

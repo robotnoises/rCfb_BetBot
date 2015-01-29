05ed503131264f4bcda8d651afcb2d3ffe6e2da0
nicholsd <nicholsd@opentext.com>
2015-01-29 16:12:53 -0500

    housekeeping

---
+++ RedditBet.API/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RedditBet.API.Repositories
+{
+    public interface IRepository<T> : IDisposable where T : class
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetWhere(Func<T, bool> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        new void Dispose();
+    }
+}
---
+++ RedditBet.API/Repository/Repository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+using System.Collections.Generic;
+
+namespace RedditBet.API.Repositories
+{
+    using RedditBet.API.Data;
+    
+    /// <summary>
+    /// Generic repository using Entity Framework
+    /// </summary>
+    /// <typeparam name="T">An EntityObject Type</typeparam>
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private DbContext _context;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public T Get(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public IEnumerable<T> GetWhere(Func<T, bool> predicate)
+        {
+            return _context.Set<T>().Where(predicate);
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
